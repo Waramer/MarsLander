6a580b9569f044f5cab32f15de220afc0dcca74f
Delezenne Quentin <quentin.delezenne@orange.fr>
2022-03-21 09:28:33 +0100

    Added Genetic initial code

--- src/Genetic.py
+++ src/Genetic.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 class Individual :
-    def __init__(self,orient,power):
-        self.orient = orient
-        self.power = power
+    def __init__(self,orient,power,nbActions):
+        self.actions = [[orient,power] for i in range(nbActions)]
+        self.fitness = -1
 
 class Genetic:
 
-    def __init__(self,nbPop,initOrient,initPower):
-        self.population = [ Individual(initOrient,initPower) for i in range(nbPop)]
+    def __init__(self,nbPop,initOrient,initPower,nbActions):
+        self.population = [ Individual(initOrient,initPower,nbActions) for i in range(nbPop)]
 
-    def evaluation(self):
-        pass
+    def evaluation(self,i,score):
+        self.population[i].fitness = score
 
     def stopCriteria(self):
         pass
